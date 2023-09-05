f7a1a8b1429d1c8657240cc8eb89814a05f47254
Hardik Shah <hardikshah_2000@yahoo.com>
2023-09-05 10:43:25 +0530

    chatbot changes

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const chatContainer = document.getElementById("chatbot-widget");
     const chatBox = document.getElementById("chat-box");
     const userInput = document.getElementById("user-input");
     const sendButton = document.getElementById("send-button");
@@ -6,22 +7,41 @@ document.addEventListener("DOMContentLoaded", function () {
     const chatbotIcon = document.getElementById("chatbot-icon");
     const chatbotPopup = document.getElementById("chatbot-popup");
 
-    chatbotIcon.addEventListener("click", function () {
-        // Toggle the class for the chatbot icon
-        chatbotIcon.classList.toggle("rectangular-icon");
+    // Initialize chatbox to be hidden
+    chatbotPopup.style.display = "none";
 
-        // Toggle the visibility of the chatbot popup
+    // Function to toggle chatbox visibility
+    function toggleChatbox() {
         if (chatbotPopup.style.display === "block") {
-            chatbotPopup.style.display = "none"; // Hide it
+            chatbotPopup.style.display = "none";
+            chatbotIcon.classList.remove("rectangular-icon");
+            chatContainer.classList.remove("rectangular-icon");
         } else {
-            chatbotPopup.style.display = "block"; // Show it
+            chatbotPopup.style.display = "block";
+            chatbotIcon.classList.add("rectangular-icon");
+            chatContainer.classList.add("rectangular-icon");
+        }
+    }
+
+    // Toggle the chatbox when clicking on the chatbot icon
+    chatbotIcon.addEventListener("click", function (event) {
+        event.stopPropagation();
+        toggleChatbox();
+    });
+
+    // Toggle the chatbox when clicking outside of the chatbox
+    document.addEventListener("click", function (event) {
+        if (!chatbotPopup.contains(event.target) && chatbotPopup.style.display === "block") {
+            toggleChatbox();
         }
     });
 
+    // Call the sendMessage function when the send button is clicked
     sendButton.addEventListener("click", function () {
-        sendMessage(); // Call the sendMessage function
+        sendMessage();
     });
 
+    // Validate user input and enable/disable the send button
     userInput.addEventListener("input", function () {
         const userMessage = userInput.value.trim();
 
@@ -43,10 +63,35 @@ document.addEventListener("DOMContentLoaded", function () {
         if (userMessage !== "") {
             appendMessage("You", userMessage);
 
-            // Simulate a response from the chatbot after a delay
-            setTimeout(() => {
-                appendMessage("Chatbot", userMessage);
-            }, 1000);
+            // Display the loader
+            const loader = document.createElement("div");
+            loader.className = "user-message-loader";
+            chatBox.appendChild(loader);
+
+            const apiUrl = `http://127.0.0.1:8000/api/web/suggestions?query=${userMessage}`;
+            console.log("apiUrl", apiUrl);
+
+            // Hit the API with the user's message
+            fetch(apiUrl, {
+                method: 'GET',
+                headers: []
+            })
+                .then(response => response.json())
+                .then(data => {
+                    // Hide the loader
+                    chatBox.removeChild(loader);
+
+                    // Display the API response as a chatbot message
+                    console.log(data);
+                    appendMessage("ChatBot", data.message);
+                })
+                .catch(error => {
+                    console.error('Error fetching API:', error);
+
+                    // Hide the loader in case of an error
+                    chatBox.removeChild(loader);
+                    appendMessage("ChatBot", "Sorry, there was an error.");
+                });
 
             userInput.value = "";
             sendButton.disabled = true;
@@ -54,25 +99,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+
+
+    // Function to append messages and separator lines
+    // Function to append messages and separator lines
     function appendMessage(sender, message) {
         const messageDiv = document.createElement("div");
         messageDiv.className = sender === "You" ? "user-message" : "bot-message";
-        messageDiv.textContent = sender + ": " + message;
-        chatBox.appendChild(messageDiv);
+        const senderElement = document.createElement("strong");
+        senderElement.textContent = sender + ": ";
 
-        // Create and append the separator line
+        // Add a class to the sender element based on the sender
+        senderElement.classList.add(sender === "You" ? "user-sender" : "bot-sender");
+
+        messageDiv.appendChild(senderElement);
+        const messageText = document.createTextNode(message);
+        messageDiv.appendChild(messageText);
+
+        chatBox.appendChild(messageDiv);
         const separatorLine = document.createElement("div");
         separatorLine.className = "message-separator";
         chatBox.appendChild(separatorLine);
-
         chatBox.scrollTop = chatBox.scrollHeight;
     }
 
-    // Close the chatbot popup when clicking outside of it
-    document.addEventListener("click", function (event) {
-        if (chatbotPopup.style.display === "block" && !chatbotPopup.contains(event.target) && event.target !== chatbotIcon) {
-            chatbotPopup.style.display = "none"; // Hide it
-            chatbotIcon.classList.remove("rectangular-icon"); // Make it circular
-        }
-    });
+
 });
+
